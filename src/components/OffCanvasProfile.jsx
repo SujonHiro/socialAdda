cdef3ec99bdf5573c6072cdb4bcd7a7c9dde2e5b
Sujon <sujonhowlader67@gmail.com>
2025-01-29 17:15:59 +0600

    fixed sideber for mobile device

--- src/components/OffCanvasProfile.jsx
+++ src/components/OffCanvasProfile.jsx
@@ -30,14 +30,6 @@ export default function OffCanvasProfile() {
         </div>
       </button>
 
-      {/* Overlay */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-[#202227] z-40"
-          onClick={toggleOffcanvas}
-        ></div>
-      )}
-
       {/* Offcanvas */}
       <div
         className={`fixed top-0 left-0 h-full w-64 bg-[#202227] shadow-lg z-50 transform transition-transform duration-300 ${
