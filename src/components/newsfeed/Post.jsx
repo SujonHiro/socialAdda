36f1bb9eebbdd3f6dcd68e718b0a6f49301dadeb
Sujon <sujonhowlader67@gmail.com>
2025-01-30 17:48:17 +0600

    Function name OffCanvas

--- src/components/newsfeed/Post.jsx
+++ src/components/newsfeed/Post.jsx
@@ -23,7 +23,7 @@ export default function Post() {
               <a href="#" className="text-sm">
                 Sam Lanson
               </a>
-              <p className="text-xs">2 Days ago</p>
+              <p className="text-xs">4 Days ago</p>
             </div>
           </div>
           <div className="p-4">
@@ -98,7 +98,7 @@ export default function Post() {
                       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"></path>
                       <path d="M11.354 4.646a.5.5 0 0 0-.708 0l-6 6a.5.5 0 0 0 .708.708l6-6a.5.5 0 0 0 0-.708"></path>
                     </svg>
-                    Block
+                    Block Profile
                   </a>
                 </li>
               </ul>
